51a07abc14649e1e61560c4ba95b402b02ce3b75
KujoFD <danny.fulmer5@gmail.com>
2025-12-21 22:10:45 -0800

    ch2L2

---
+++ pythonAIagent/functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        #bool
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if valid_target_dir == False:
+            return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        if os.path.isdir(target_dir) == False:
+            return(f'Error: "{directory}" is not a directory')
+
+        itemlist = []
+    
+        for item in os.listdir(target_dir):
+            working_dir=os.path.join(target_dir, item)
+            itemlist.append(f'- {item}: file_size={os.path.getsize(working_dir)} bytes, is_dir={os.path.isdir(working_dir)}')
+    except Exception as e:
+            return(f"Error: {e}")
+
+    return "\n".join(itemlist)
+
